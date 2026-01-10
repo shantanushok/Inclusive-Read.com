d5b0b136dab59883b8d85fe787d965d3f8ccb35a
Shantanu <shantanuv.03@gmail.com>
2026-01-10 23:38:28 +0530

    comic sans looks better

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['PT Sans', 'sans-serif'],
+        body: ['Comic Sans MS', 'Comic Sans', 'cursive'],
+        headline: ['Comic Sans MS', 'Comic Sans', 'cursive'],
         code: ['monospace'],
       },
       colors: {
